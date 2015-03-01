601be73dd5f5377bba6ce327f696a1b0bd11d5cf
Oliver Martell <oliver.martell@gmail.com>
2015-03-01 18:43:56 +0000

    Draw one of the known cyclic patterns

--- src/cljs/life/core.cljs
+++ src/cljs/life/core.cljs
@@ -1,19 +1,5 @@
 (ns life.core
-  (:require [om.core :as om :include-macros true]
-            [om.dom :as dom :include-macros true]
-            [clojure.set :as set]))
-
-(defonce app-state (atom))
-
-(enable-console-print!)
-
-(defn main []
-  (draw-cells))
-
-(defn draw-cells []
-  (let [context (.getContext (.getElementById js/document "canvas") "2d")]
-    (doseq [n (take 10 (repeatedly 1)) ]
-      (.fillRect context (* n 15) (* n 15) 10 10))))
+  (:require [clojure.set :as set]))
 
 (defn neighbors-locations [[x y]]
   (into #{} (for [sumx #{-1 1 0} sumy #{-1 1 0}
@@ -22,14 +8,14 @@
 
 (defn neighbors-per-cell [world]
   (reduce
-   (fn [map cell]
+   (fn [m cell]
      (let [cell-neighbors (neighbors-locations cell)]
-       (assoc map cell {:living (set/intersection cell-neighbors world)
-                        :dead (set/difference cell-neighbors world)})))
+       (assoc m cell {:living (set/intersection cell-neighbors world)
+                      :dead (set/difference cell-neighbors world)})))
    {}
    world))
 
-(defn living-cells [neighbors-per-cell]
+(defn survivor-cells [neighbors-per-cell]
   (keys (filter (fn [[cell neighbors]]
                   (#{2 3} (count (:living neighbors))))
                 neighbors-per-cell)))
@@ -44,5 +30,47 @@
 
 (defn tick [world]
   (let [neighbors-per-cell (neighbors-per-cell world)]
-    (set/union (into #{} (living-cells neighbors-per-cell))
+    (set/union (into #{} (survivor-cells neighbors-per-cell))
                (into #{} (new-born-cells neighbors-per-cell)))))
+
+(defn draw-cells [world]
+  (let [element (.getElementById js/document "canvas")
+        context (.getContext element "2d")]
+    (.clearRect context 0 0 500 500)
+    (doseq [x (range 100)
+            y (range 100)]
+      (.strokeRect context (* x 11) (* y 11) 11 11))
+    (set! (. context -fillStyle) 'green')
+    (doseq [[x y] world]
+      (.fillRect context (* x 11) (* y 11) 10 10))))
+
+(defn draw-next-tick [world]
+  (.setTimeout js/window
+               (fn []
+                 (let [next-generation (tick world)]
+                   (draw-cells next-generation)
+                   (draw-next-tick next-generation)))
+               2000))
+
+(defn main []
+  (let [world #{[12,0] [13,0] [14,0]
+                [12,5] [13,5] [14,5]
+                [10,2] [10,3] [10,4]
+                [15,2] [15,3] [15,4]
+
+                [17,2] [17,3] [17,4]
+                [18,0] [19,0] [20,0]
+                [18,5] [19,5] [20,5]
+                [22,2] [22,3] [22,4]
+
+                [12,7]  [13,7]  [14,7]
+                [12,12] [13,12] [14,12]
+                [10,8]  [10,9]  [10,10]
+                [15,8]  [15,9]  [15,10]
+
+                [17,8]  [17,9]  [17,10]
+                [18,7]  [19,7]  [20,7]
+                [18,12] [19,12] [20,12]
+                [22,8]  [22,9]  [22,10]}]
+    (draw-cells world)
+    (draw-next-tick world)))
--- test/life/test/core_test.cljs
+++ test/life/test/core_test.cljs
@@ -6,10 +6,16 @@
             [clojure.set :as set]))
 
 ;; Cell Coordinates
-;; [0,0 1,0 2,0 3,0
-;;  0,1 1,1 2,1 3,1
-;;  0,2 1,2 2,2 3,2
-;;  0,3 1,3 2,3 3,3]
+;; [0,0 1,0 2,0 3,0 4,0 5,0 6,0 7,0 8,0
+;;  0,1 1,1 2,1 3,1 4,1 5,1 6,1 7,1 8,1
+;;  0,2 1,2 2,2 3,2 4,2 5,2 6,2 7,2 8,2
+;;  0,3 1,3 2,3 3,3 4,3 5,3 6,3 7,3 8,3
+;;  0,4 1,4 2,4 3,4 4,4 5,4 6,4 7,4 8,4
+;;  0,5 1,5 2,5 3,5 4,5 5,5 6,5 7,5 8,5
+;;  0,6 1,6 2,6 3,6 4,6 5,6 6,6 7,6 8,6
+;;  0,7 1,7 2,7 3,7 4,7 5,7 6,7 7,7 8,7
+;;  0,8 1,8 2,8 3,8 4,8 5,8 6,8 7,8 8,8]
+
 (deftest a-tick-produces-the-next-generation
   (testing "A cell with no neighbors dies on the next tick"
     (is (= #{}
@@ -22,20 +28,32 @@
   (testing "A cell with two or three living neighbors lives on the next tick"
     (testing "one neighbor on top and one under"
       (is (set/subset? #{[1 1]}
-                   (life/tick #{[1 1] [1 0] [1 2]}))))
+                       (life/tick #{[1 1] [1 0] [1 2]}))))
 
     (testing "one neighbor left and one right"
       (is (set/subset? #{[1 2]}
-             (life/tick #{[1 2] [0 2] [2 2]}))))
+                       (life/tick #{[1 2] [0 2] [2 2]}))))
 
     (testing "three neighbors including diagonals"
       (is (set/subset? #{[1 2] [0 2] [2 2] [1 3]}
-                   (life/tick #{[1 2] [0 2] [2 2] [1 3]})))))
+                       (life/tick #{[1 2] [0 2] [2 2] [1 3]})))))
 
   (testing "A cell with more than three neighbors dies by overpopulation"
     (is (set/subset? #{[0 2] [2 2] [1 3] [1 1]}
-                 (life/tick #{[1 2] [0 2] [2 2] [1 3] [1 1]}))))
+                     (life/tick #{[1 2] [0 2] [2 2] [1 3] [1 1]}))))
 
   (testing "A new cell is born when there are three neighbors"
     (is (= #{[1 1] [0 1] [2 1]}
            (life/tick #{[1 0] [1 1] [1 2]})))))
+
+(deftest known-patterns
+  (testing "A known pattern"
+    (is (= #{[13 -1] [13 0] [13 1] [14 1]
+             [9 3] [10 3] [11 3] [11 4]
+             [14 3] [15 3] [16 3] [15 4]
+             [13 4] [13 5] [14 5] [13 6]}
+
+           (life/tick #{[12,0] [13,0] [14,0]
+                        [12,5] [13,5] [14,5]
+                        [10,2] [10,3] [10,4]
+                        [15,2] [15,3] [15,4]})))))
